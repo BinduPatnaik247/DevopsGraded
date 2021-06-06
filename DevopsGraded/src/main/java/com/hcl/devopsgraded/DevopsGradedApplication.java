cb9189d1b9d45ce2cfce33f5da26d1cff2b87f82
BinduPatnaik247 <84894669+BinduPatnaik247@users.noreply.github.com>
2021-06-06 14:58:15 +0530

    Update DevopsGradedApplication.java

--- DevopsGraded/src/main/java/com/hcl/devopsgraded/DevopsGradedApplication.java
+++ DevopsGraded/src/main/java/com/hcl/devopsgraded/DevopsGradedApplication.java
@@ -8,6 +8,8 @@ public class DevopsGradedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsGradedApplication.class, args);
+		private static Logger logger=LoggerFactory.getLogger.getLogger(DevopsGradedApplication.class);
+		logger.info("Hello DevOps");
 		System.out.println("Hello Devops");
 	}
 
